6ce83f7e6e3d755a953380cb93e62acfcca570fc
Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
2018-10-31 15:40:38 +0900

    Update webui
    Signed-off-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>

--- webui.py
+++ webui.py
@@ -1,17 +1,15 @@
 # -*- coding:utf-8 -*-
 
 import sqlite3
-from bottle import route, run
+from bottle import route, run, template
 
-@route('/pkginfo')
-def pkginfo():
+dbpath = './pkgdb.sqlite'
+codename = 'rocko'
+deb_codename = 'buster'
+base_table = "pkginfo_%s_%s" % (codename, deb_codename)
+deb_table = "pkginfo_%s" % (deb_codename)
 
-    dbpath = './pkgdb.sqlite'
-    codename = 'rocko'
-    deb_codename = 'buster'
-    base_table = "pkginfo_%s_%s" % (codename, deb_codename)
-    deb_table = "pkginfo_%s" % (deb_codename)
-    result_data = ''
+def get_pkgs_list():
 
     connection = sqlite3.connect(dbpath)
     cursor = connection.cursor()
@@ -21,6 +19,7 @@ def pkginfo():
     cursor.execute(sql_exec)
     base_rows = cursor.fetchall()
     
+    pkgs_list = []
     for base_row in base_rows:
         if len(base_row) == 3 and base_row[2] != '':
             deb_pkg_name = base_row[2]
@@ -41,14 +40,25 @@ def pkginfo():
         if poky_pkg_name == deb_pkg_name:
             deb_pkg_name = "-"
 	
-        result_data = result_data + ("%s, %s, %s, %s<br>" % (poky_pkg_name, poky_pkg_ver, deb_pkg_name, deb_pkg_ver))
         # print ("%s, %s, %s, %s" % (poky_pkg_name, poky_pkg_ver, deb_pkg_name, deb_pkg_ver))
-    
+
+        pkgs_list.append ({
+            "poky": poky_pkg_name,
+            "poky_ver": poky_pkg_ver,
+            "deb": deb_pkg_name,
+            "deb_ver": deb_pkg_ver
+        })
+
     # finish
     connection.commit()
     connection.close()
 
-    return result_data
+    return pkgs_list
+
+@route("/")
+def index():
+    pkgs_list = get_pkgs_list()
+    return template("index", pkgs_list=pkgs_list)
 
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True)
